6cc9bc87d6d1bc51f826539f2fced89689b3d167
Mohammed Taqi <taqealibade@gmail.com>
2023-12-17 20:37:38 +0300

    first comment

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import { createRouter,  createWebHistory} from 'vue-router'
+import './style.css'
+import App from './App.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    //here where we will define our routes
+    routes:
+})
+
+createApp(App).mount('#app')
