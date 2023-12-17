376db8490fa8b6e83734f578a4ca010db545d94e
Mohammed Taqi <taqealibade@gmail.com>
2023-12-17 22:02:02 +0300

    finishing the destinations routes

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,17 @@
 </script>
 
 <template>
-  <!-- we use the router link tag for any internal link -->
-  <router-link to="/">Home</router-link>
-  <br>
-  <router-link to="/about">About</router-link>
-  <router-view>
+  <div id="nav">
+    <!-- we use the router link tag for any internal link -->
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>
 
-  </router-view>
+  <div class="container">
+    <router-view></router-view>
+  </div>
 </template>
-
-<!-- <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #9a0845aa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style> -->
