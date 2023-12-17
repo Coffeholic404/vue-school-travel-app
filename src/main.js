c2b317d9117030e03ce92dc3d498fb41dbc28e8c
Mohammed Taqi <taqealibade@gmail.com>
2023-12-17 21:07:35 +0300

    creating the Home page

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import { createRouter,  createWebHistory} from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import Home from '@Home.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     //here where we will define our routes
-    routes: []
+    routes: [
+        {path: '/', name: 'Home', component: Home}
+    ]
 })
 
 createApp(App)
