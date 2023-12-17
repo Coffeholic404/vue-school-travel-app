3b8cff28a4da33bff7bedb98f5e8016bdb55c743
Mohammed Taqi <taqealibade@gmail.com>
2023-12-17 21:10:04 +0300

    creating the about page

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import { createRouter,  createWebHistory} from 'vue-router'
 import './style.css'
 import App from './App.vue'
 import Home from '@Home.vue'
+import About from '@About.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     //here where we will define our routes
     routes: [
-        {path: '/', name: 'Home', component: Home}
+        {path: '/', name: 'Home', component: Home},
+        {path: '/about', name: 'About', component: About},
     ]
 })
 
